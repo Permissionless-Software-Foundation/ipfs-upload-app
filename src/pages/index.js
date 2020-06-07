9c288247db99b6e214e682c59293b6e52b925cca
Chris Troutner <chris.troutner@gmail.com>
2020-06-07 07:24:00 -0700

    Adding description

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,6 +20,38 @@ class Upload extends React.Component {
                 {/* <Banner /> */}
                 <UploadForm />
 
+                <div className="grid-wrapper">
+                  <div className="col-2"></div>
+                  <div className="col-8">
+                    <h2><u>About</u></h2>
+                    <p>
+                      This simple web app allows you to easily upload files to be
+                      hosted on
+                      the <a href="https://ipfs.io" target="_blank" rel="noreferrer">IPFS network</a>.
+                      The cost is $0.01 USD per 10 Megabytes, with a minimum cost
+                      of $0.01. This kind of low-cost, permissionless payment service
+                      is only possible with Bitcoin Cash.
+                    </p>
+                    <p>
+                      This web app is open source, and uses an MIT license.
+                      You can copy it and run your
+                      own hosting service. There is a front-end and a back-end:
+
+                      <ul>
+                        <li>
+                          <a href="https://github.com/Permissionless-Software-Foundation/ipfs-upload-app" target="_blank" rel="noreferrer">
+                            Front End App</a>
+                        </li>
+                        <li>
+                          <a href="https://github.com/Permissionless-Software-Foundation/ipfs-upload-server" target="_blank" rel="noreferrer">
+                            Back End App</a>
+                        </li>
+                      </ul>
+                    </p>
+
+                  </div>
+                  <div className="col-2"></div>
+                </div>
             </Layout>
         )
     }
